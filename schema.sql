5e83ca1a5ad92489634823d13bf106594fd4ec96
Scott Schneider <schneids2524@gmail.com>
2021-06-12 12:03:14 -0700

    Updates

--- schema.sql
+++ schema.sql
@@ -1,16 +1,28 @@
 -- ETL-Challenge
--- Create tables and imort files
+-- Create tables and import files
 
 CREATE TABLE Cryptocurrency (
-	Symbol VARCHAR(20) NOT NULL,
-	Date Date NOT NULL,
-	Open INTEGER NOT NULL,
-	High INTEGER NOT NULL,
-	Low INTEGER NOT NULL,
-	Close INTEGER NOT NULL,
-	Volume INTEGER NOT NULL,
-	Market Cap INTEGER NOT NULL,
-	PRIMARY KEY (Symbol)
+	"Symbol" VARCHAR(20) NOT NULL,
+	"Date" Date NOT NULL,
+	"High" INTEGER NOT NULL,
+	"Low" INTEGER NOT NULL,
+	"Open" INTEGER NOT NULL,
+	"Close" INTEGER NOT NULL,
+	"Volume" INTEGER NOT NULL,
+	"Marketcap" INTEGER NOT NULL
+	-- PRIMARY KEY ("Symbol")
 );
 
 SELECT * FROM Cryptocurrency;
+
+DROP TABLE Cryptocurrency;
+
+CREATE TABLE SP500 (
+	"Symbol" VARCHAR(20) NOT NULL,
+	"DATE" DATE NOT NULL,
+	"SP500" INTEGER NOT NULL
+);
+
+SELECT * FROM SP500;
+
+DROP TABLE SP500;
