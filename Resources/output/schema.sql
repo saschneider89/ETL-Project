385ffcb541c43af7cbfe80660bb22bb1e1bb70d9
Fannie Chang <fanniech@gmail.com>
2021-06-12 02:12:16 -0700

    data retrieve from yahoo and fred

---
+++ Resources/output/schema.sql
@@ -0,0 +1,116 @@
+CREATE TABLE SP500(
+	symbol varchar(10),
+	date date not null,
+	sp500 decimal not null);
+	
+create table ada( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+
+create table BNB( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table BTC( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+create table DOGE( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table DOT( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table ETH( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table UNI( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table USDC( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table USDT( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+create table XRP( 
+	symbol varchar(10),
+	date date not null,
+	high decimal not null,
+	low decimal not null,
+	open_p decimal not null,
+	close_p decimal not null,
+	volume decimal not null,
+	adj_close decimal not null
+	);
+	
+	
+
